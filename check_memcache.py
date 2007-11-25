f6a02d48cf26ea4247fa556c8334baa61d7e1f1b
kakikubo <kakikubo@cf2651cb-8824-0410-88cd-b1deea40b454>
2007-11-25 12:13:53 +0000

    Nagios Plugins . This Plugins that checks the memcached server . beta version
    git-svn-id: file:///Users/kakikubo/SVHack/trunk/Hack/CheckScript@678 cf2651cb-8824-0410-88cd-b1deea40b454

--- check_memcache.py
+++ check_memcache.py
@@ -1,43 +1,61 @@
 #! /usr/bin/env python
 '''
-check_memcache.py 
-set the temporary key and delete
+
+  2007 Kakikubo Teruo
+  check_memcache.py 
+
+  Check Memcached Server plugin. 
+  This Plugin requires the memcached-Python API. 
+  This Plugin was tested only in the python 2.5 
 
 '''
 import memcache
 import signal
-import os
+import os,sys
 
 from optparse import OptionParser
+
 pid = str(os.getpid())
 # option
 parser = OptionParser()
 parser.add_option("-H","--hostname",dest="hostaddress",
-                  help="Host Address", metavar="hostaddr")
+                  help="Host Address", metavar=" <hostaddr>")
 parser.add_option("-t","--timeout",dest="timeout",
-                  help="timeout x seconds", metavar="timo", default=10)
+                  help="timeout x seconds", metavar=" <timeout>", default=10)
 parser.add_option("-p","--port",dest="port",
-                  help="port number", metavar="pt" , default=11211)
+                  help="port number", metavar=" <port number>" , default=11211)
 (opts,args)=parser.parse_args()
 
 if not opts.hostaddress:
     parser.print_help()
-    parser.error("Please specify Host")
+    parser.error("UNKNOWN - Please specify Host")
     exit(2)
 
 svr = u""
 svr = str(opts.hostaddress) + ":" + str(opts.port)
-mc = memcache.Client([ svr ], debug=1)
+mc = memcache.Client([ svr ], debug=0)
 #mc = memcache.Client({opts.hostaddress}:{opts.port}, debug=1)
 #mc = memcache.Client(["ocnblg-tc01-int:112"], debug=1)
+
+def handler(signum, frame):
+    #print 'Signal handler called with signal', signum
+    #raise IOError, "hogehogehgoehgoe"
+    print "CRITICAL - timeout after " + str(opts.timeout) + " seconds"
+    sys.exit(2)
+
+## Set the signal handler
+signal.signal(signal.SIGALRM, handler)
+signal.alarm(int(opts.timeout))
+
 if not mc.set(pid,"nagios value"):
-    raise ValueError,"CRITICAL - cannot set the value"
-    exit(2)
+    print "CRITICAL - cannot set the value"
+    sys.exit(2)
 else:
     if not mc.get(pid) == 'nagios value':
-        raise ValueError, "CRITICAL - cannot get the value"
-        exit(2)
+        print "CRITICAL - cannot get the value"
+        sys.exit(2)
     else:
 	mc.delete(pid)
 	print "OK - memcached alive"
-        exit 
+        sys.exit(0)
+
